462dba1b51ca0b6e3efca8a1958e69d574f93cc2
durmusgulbahar <durmus.gulbahar@outlook.com>
2022-10-10 01:34:45 +0300

    finished

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
+
+const {API_KEY, PRIVATE_KEY} = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  networks:{
+    goerli:{
+      url:API_KEY,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan:{
+    apiKey: {
+      goerli : "N31ZW5IWSUW315WPRDGJ95RMSBVKCWEJ95",
+    }
+  }
 };
