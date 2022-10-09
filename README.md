009652fe4843b29a1e346e3ab3ea074e7413867d
durmusgulbahar <durmus.gulbahar@outlook.com>
2022-10-10 00:42:43 +0300

    final

--- README.md
+++ README.md
@@ -4,7 +4,26 @@
 
 ### PICK THE NUMBER GAME
 
+1. Oyuncu oyuna girişte bir ücret(0.005 ether) öder ve 0-9 arası bir sayı seçer. 
+    - Oyuna giriş eventi tetiklenir.
+2. Maksimum oyuncu sayısına ulaşıldığında otomatik olarak oyun başlar. 
+    - Oyun başladı eventi tetiklenir.
+3. İlk olarak Chainlink'ten rastgele bir uint256 sayı alınır.
+    - RequestSent eventi tetiklenir.
+4. Bu rastgele sayının ilk basamağı bizim **Şanslı Sayı**mızdır. 
+5. Bu sayının belirlenmesi yaklaşık 1-1.5 dakika alıyor.
+    - RequestFulfilled eventi tetiklenir.
+6. Oyuncularımız ve seçtikleri sayı bir mapping'de tutulur
+    > **mapping(uint -> address) playersAndSelectedNumbers** yani her sayının kendine ait bir addres listesi vardır. Bu listede
+    belirtilen sayıyı seçen oyuncular bulunur. Örnek olarak :
+    2 -> [0x241924129314928, 0x241924129314923] gibi.
+7.  **Şanslı Sayı**mız belirlendikten sonra **playersAndSelectedNumbers** isimli mappinginden şanslı sayımızı alırız. Her sayı oyuncu listesi tuttuğu için bir for döngüsü kullanmayız. Direkt bu listeyi çekebiliriz.
+8.  Kasa bakiyemiz, kazananların sayısına bölünerek kişi başı ödül hesaplanır.
+9.  Bu kişi başı ödül her kazanana contract tarafından gönderilir.
+10. Eğer kazanan yoksa ödül bir sonraki oyuna devreder.
+11. Oyun bitişinde oyun verileri bir **Game** struct'ında tutulur ve **mapping(uint -> Game) games** isimli mappingimizde depolanır. 
+12. Oyun bütün adımlar sırasıyla otomatik bir şekilde birbirini tetikleyecek şekilde dizayn edilmiştr.
 
 
 
-by https://app.patika.dev/durmuss
+by Durmuş Gülbahar / https://app.patika.dev/durmuss
--- hardhat/contracts/PickTheNumber.sol
+++ hardhat/contracts/PickTheNumber.sol
@@ -2,8 +2,9 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 /**
  *      -PickTheNumber bir şans oyunu. Kullanıcılar oyuna katılırken bir sayı seçer, giriş ücretini
@@ -13,7 +14,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * alır ve bize 77 digitlik bir uint256 sayı verir. Bunun ilk basamağı bizim şanslı ve kazanan sayımız
  * olacaktır.
  *      -Katılan oyuncular maksimum sayıya ulaştığında oyun otomatikman başlar
- *      -Contract Chainlink coordinator'una istek atacağı için her zaman LINK bulunmalıdır.
+ *      -Kontrat vrf.chainlink tarafından kullanılıyor. Chainlink VRF2 kullanımı :
+ *          --Chainlink VRF2 bir hesabın açtığı fund ile o fund'un subscription ID'sini kullanan
+ *          --kontratlara LINK gönderiyor. Böylece kontratın LINK depolama zorunluluğu kalkmış oluyor.
+ *          --ve her işlemde LINK göndermediği için tasarruf sağlanıyor.
  *      -Kazanan sayı belirlendiği zaman oyuncu listesini mapping sayesinde alabilirz.
  *      -Bir döngü yardımıyla kazananlar listesine sırayla hesaplanmış ödüller gönderilir.
  *      -Kişi başı ödül -> (maxPlayer * entryFee) / (winner.length) olarak hesaplanır.
@@ -25,22 +29,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  *
  */
 
-/**
- *  **** Fonskyionların public/private ları ayarlanacak, 
- *  **** Contract ikinci oyuncuyu kabul etmiyor
- *  **** Eğer kimse kazanamazsa bir sistem kur
- *  **** 
- */
-contract PickTheNumber is VRFConsumerBase {
-    //Chainlink değişkenleri
-    uint256 public vrf_fee = 0.005 ether; //VRF Coordinatorüne gönderdiğimiz ücret.
-    bytes32 public keyHash; //Coordinator ve aramızda yapılan bağıntı.
+contract PickTheNumber is VRFConsumerBaseV2, ConfirmedOwner {
+    // @@@@@Chainlink değişkenleri
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/#configurations
+    bytes32 keyHash =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // 1 Adet random sayı döndürüyor.
+    uint32 numWords = 1;
+
+    uint64 s_subscriptionId; // Subscription ID.
+
+    VRFCoordinatorV2Interface COORDINATOR;
 
     uint public _entryFee = 0.005 ether; // Oyuncular oyuna katılmak için ödemeliler.
 
     mapping(uint256 => Game) public games; // Game değişkenlerini tutan mapping.
 
-    uint256[] keysOfGames; // Arayüzde kolayca "games" mapping değişkenlerini çekebilmek için
+    uint256[] public keysOfGames; // Arayüzde kolayca "games" mapping değişkenlerini çekebilmek için
     // keylerini burada listeliyoruz böylece kolayca bir döngüyle Game verilerini çekebileceğiz.
 
     uint256 private gameCounter = 1; // gameCounter, her Game değişkeninin gameID'sine eşittir
@@ -55,23 +74,21 @@ contract PickTheNumber is VRFConsumerBase {
     address[] public winners; // Girişte en çok tekrar eden sayıyı bulanlar
     uint public totalReward; // Oynanmakta olan oyunun ödül havuzu
     bool public isGameStarted;
-    uint8 public constant maxPlayers = 2;
-
+    uint8 public constant maxPlayers = 3; // Maksimum oyuncu sayısına ulaşıldığı an oyun başlar.
 
     // 1 => [0x1, 0x2 ,0x3] böylece bir sayı kazandığında
     // bu addreslerin hepsine ödül göndericez.
     // winnersı buradan çekicez.
     mapping(uint => address[]) public playersAndSelectedNumbers; // 0-9 olan sayıları seçen address listeleri
 
-    
-    constructor(
-        address _vrfCoordinator,
-        address linkToken,
-        
-        bytes32 _keyHash
-    ) VRFConsumerBase(_vrfCoordinator, linkToken) {
-        
-        keyHash = _keyHash;
+    constructor(uint64 subscriptionId)
+        VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+        ConfirmedOwner(msg.sender)
+    {
+        s_subscriptionId = subscriptionId;
+        COORDINATOR = VRFCoordinatorV2Interface(
+            0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
+        );
         isGameStarted = false;
     }
 
@@ -83,17 +100,28 @@ contract PickTheNumber is VRFConsumerBase {
         address[] winners; // Oyunun kazananları
     }
 
-    event EnterTheGame(uint indexed gameID, address participant);
+    event EnterTheGame(
+        uint indexed gameID,
+        address participant,
+        uint selectedNumber
+    ); // oyuna katılış
 
-    event GameStart(uint indexed gameID);
+    event GameStart(uint indexed gameID); // Oyun başlangıcı
 
-    event GameEnd(uint indexed gameID, uint luckyNumber, address[] winners);
+    event GameEnd(uint indexed gameID, uint luckyNumber, address[] winners); // oyun bitişi
 
-    event PickedTheNumber(uint indexed gameID, address player, uint number);
+    event NoWinner(uint indexed gameID, uint totalReward);
 
-    //HAZIRLIK EVRESİ
+    //Chainlink events
+    event RequestSent(uint256 requestId, uint32 numWords);
+
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    //@@@@@@@@ HAZIRLIK EVRESİ
+
     /** 
     @dev Kullanıcı, entryFee yi öder ve bir sayı seçerek oyuna giriş yapar.
+    @param _selectedNumber - Oyuncunun seçtiği sayı.
     */
     function enterGame(uint _selectedNumber) external payable {
         require(!isGameStarted, "Game is not started yet");
@@ -106,18 +134,16 @@ contract PickTheNumber is VRFConsumerBase {
 
         playersAndSelectedNumbers[_selectedNumber].push(msg.sender);
 
-        emit PickedTheNumber(gameCounter, msg.sender, _selectedNumber);
-
-        emit EnterTheGame(gameCounter, msg.sender);
+        emit EnterTheGame(gameCounter, msg.sender, _selectedNumber);
 
         if (gameParticipants.length == maxPlayers) {
             startGame();
         }
     }
 
-    //OYUN
+    //@@@@@@@@@@@ OYUN EVRESİ
 
-    /*
+    /**
     @dev Oyunu başlatır. İlk olarak Chainlink aracılığıyla 0-15 arasında 16 tane numara rastgele olarak seçilir.
          Bu rastgele üretilen sayılardan en fazla tekrar edeni bulunur.
 
@@ -128,12 +154,21 @@ contract PickTheNumber is VRFConsumerBase {
 
         emit GameStart(gameCounter);
 
-        getLuckyNumberFromChainlink();
+        requestRandomWords();
     }
 
-    function getLuckyNumberFromChainlink() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= vrf_fee, "Not enough LINK");
-        return requestRandomness(keyHash, vrf_fee);
+    function requestRandomWords() internal returns (uint256 requestId) {
+        // Eğer subscription ayarlanmamışsa ve bakiye yetersizse hata verir.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+
+        emit RequestSent(requestId, numWords);
+        return requestId;
     }
 
     //OYNANIŞ PİPELİNE
@@ -141,13 +176,13 @@ contract PickTheNumber is VRFConsumerBase {
      * @dev randomness bize 77 digitlik bir random uin256 sayısı veriyor. Biz bunun ilk 16 basamağını alacağız.
      * @dev override fonksiyon
      */
-    function fulfillRandomness(bytes32 requestId, uint256 randomness)
-        internal
-        virtual
-        override
-    {
-        uint256 winnerNumber = randomness / 10**76; // İlk digitni aldık.
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        uint256 winnerNumber = _randomWords[0] / 10**76; // gelen random sayının ilk digitni aldık
         luckyNumber = winnerNumber;
+        emit RequestFulfilled(_requestId, _randomWords);
         setWinners(winnerNumber);
     }
 
@@ -157,30 +192,38 @@ contract PickTheNumber is VRFConsumerBase {
     function setWinners(uint _winnerNumber) public {
         winners = playersAndSelectedNumbers[_winnerNumber];
 
-        sendRewards();
+        if (winners[0] == address(0)) {
+            finishTheGameAndStoreGameData();
+            emit NoWinner(gameCounter, getTotalReward());
+            revert("Kazanan yok, kasa bir sonraki oyuna devredildi.");
+        }
+
+        sendRewards(winners);
     }
 
     /**
      * @dev Ödül havuzunu kazanan liste uzunluğuna bölüyoruz ve kişi başı kazanılan ödülü hesaplıyoruz
      * winner listesinde olan her kişiye sıra sıra bu ödülü gönderiyoruz.
      */
-    function sendRewards() internal {
-        uint rewardForEach = totalReward / winners.length;
+    function sendRewards(address[] memory _winners) internal {
+        uint rewardForEach = getTotalReward() / _winners.length;
 
-        for (uint i = 0; i < winners.length; i++) {
-            (bool sent, ) = winners[i].call{value: rewardForEach}("");
+        for (uint i = 0; i < _winners.length; i++) {
+            (bool sent, ) = _winners[i].call{value: rewardForEach}("");
             require(sent, "Failed to send reward");
         }
 
         finishTheGameAndStoreGameData();
     }
 
-    //BİTİŞ
+    /**
+     * @dev Oyun biter ve oyunun verileri "games" mapping'ine kaydedilir.
+     */
     function finishTheGameAndStoreGameData() private {
         Game memory finishedGame = Game({ //GAME STRUCT kayıt olarak kullanılacak. Oyun bittikten sonra oluşturulacak.
             gameID: gameCounter,
             luckyNumber: luckyNumber,
-            participants: gameParticipants, //Get participants from function as array
+            participants: gameParticipants, // Katılımcılar
             totalReward: getTotalReward(),
             winners: winners // getWinners()
         });
@@ -195,7 +238,7 @@ contract PickTheNumber is VRFConsumerBase {
     }
 
     /**
-     * @dev Reset the game and ready to start new game.
+     * @dev Oyun verilerini sıfırlar ve yeni oyuna hazırlık yapar.
      */
     function beReadyToNewGame() internal {
         for (uint i = 0; i < gameParticipantsCounter; i++) {
@@ -206,17 +249,17 @@ contract PickTheNumber is VRFConsumerBase {
         delete gameParticipantsCounter; //Katılımcı sayısı
         delete luckyNumber; // 0-9 arası rastegele seçilmiş rastgele bir sayı
         delete winners; // Girişte en çok tekrar eden sayıyı bulanlar
-        delete totalReward; // Oynanmakta olan oyunun ödül havuzu
-        delete isGameStarted;
+        totalReward = address(this).balance; // Oynanmakta olan oyunun ödül havuzu
         isGameStarted = false;
     }
 
-    /*
-    @@@@@ GETTERS
-    */
+    /**
+     * GETTERS
+     * @dev Oyun verilerinin getter fonksiyonları.
+     */
 
     function getTotalReward() public view returns (uint) {
-        return getPlayerList().length * _entryFee;
+        return address(this).balance;
     }
 
     function getWinners() public view returns (address[] memory) {
@@ -233,10 +276,6 @@ contract PickTheNumber is VRFConsumerBase {
 
     function totalPlayersCurrentGame() public view returns (uint) {
         return gameParticipantsCounter;
-    }
-
-    function getLinkBalance() public view returns (uint){
-        return LINK.balanceOf(address(this));
     }
 
     receive() external payable {}
